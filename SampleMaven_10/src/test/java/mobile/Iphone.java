d2e6ff54c11ce343bfb2c932c714f2d4dfe9c07e
vipen <vipen@Hi-Speed-S340>
2025-02-02 18:09:52 +0530

    mobile commit

---
+++ SampleMaven_10/src/test/java/mobile/Iphone.java
@@ -0,0 +1,14 @@
+package mobile;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Iphone {
+
+	@Test
+	
+	public void iphone()
+	{
+		Reporter.log("iphone got executed",true);
+	}
+}
---
+++ SampleMaven_10/src/test/java/mobile/Samsung.java
@@ -0,0 +1,16 @@
+package mobile;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+
+public class Samsung {
+
+	@Test
+	
+	public void samsung()
+	{
+		Reporter.log("samsung mobile got executed",true);
+	}
+}
